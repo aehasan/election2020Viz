de076f1e377b3e0516e964102a1d588750aa133f
ahmed <aehasan2@illinois.edu>
2020-12-16 23:42:03 -0600

    initial setup

---
+++ visualization.js
@@ -0,0 +1,91 @@
+var svg = d3.select("#map")
+        .append("svg")
+        .attr("height", 500)
+        .attr("width", 1000)
+        .append("g");
+var promises = [];
+var projection = d3.geoAlbersUsa();
+
+var path = d3.geoPath().projection(projection);
+var fipsToVotes = {};
+
+
+var demColor = d3.scaleThreshold().domain([1,1.3,1.5,1.7,2]).range(["#ccccff", "#8080ff", "#3333ff", "#0000e6", "#000033" ]);
+var repColor = d3.scaleThreshold().domain([1,1.3,1.5,1.7,2]).range(["#ffb3b3", "#ff6666", "#ff1a1a", "#cc0000", "#8b0000" ]);
+
+
+async function test() {
+    
+    j =   await d3.csv("https://raw.githubusercontent.com/MEDSL/2018-elections-unoffical/master/election-context-2018.csv").then(
+    function(data) {
+ 
+         
+        data.forEach(function(d) {
+            //console.log(d.clinton16)
+            if (d.fips.length > 4) {
+            fipsToVotes[d.fips] = [d.clinton16, d.trump16];
+            } else {
+                fipsToVotes["0" + d.fips] = [d.clinton16, d.trump16];
+
+            }
+
+        })
+
+
+    });
+
+    d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json").then(
+    function(data) {
+        //console.log(data.objects.counties.geometries[0].properties.name)
+
+        var states = topojson.feature(data, data.objects.counties).features
+
+        //console.log(states)
+        svg.selectAll(".states")
+            .data(states)
+            .enter()
+            .append("path")
+            .attr("class" , "states")
+            .attr("d", path)
+            .attr("fill", function(data) {
+                var temp = fipsToVotes[data.id]
+            if (temp != undefined) {
+                //console.log(temp[0])
+                if (data.id == 17031) {
+                    console.log(fipsToVotes[data.id])
+                    console.log(data)
+
+                }
+                var tempa = fipsToVotes[data.id][0]
+                var tempb = fipsToVotes[data.id][1]
+                if (Number(tempa) >= Number(tempb)) {
+
+                    var first = Number(tempa) / Number(tempb)
+
+
+                    return demColor(first)
+
+                } else if (Number(fipsToVotes[data.id][0]) < Number(fipsToVotes[data.id][1])) {
+                    var tempa1 = fipsToVotes[data.id][0]
+                    var tempb2 = fipsToVotes[data.id][1]
+                    var first = Number(tempb2) / Number(tempa1)
+
+                    return repColor(first)
+  
+                } 
+            }
+                console.log(fipsToVotes[data.id])
+
+                return "#f2f0f7"
+            })
+            .on("mouseover", function(d) {
+                
+            })
+        }
+    );
+}
+
+test();
+
+
+
