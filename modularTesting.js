c3ba58defe23fc8b08ece755243891fe9caabcb4
ahmed <aehasan2@illinois.edu>
2020-12-19 22:46:52 -0600

    added files for mod testing

---
+++ modularTesting.js
@@ -0,0 +1,51 @@
+const sample = [
+    {
+      language: 'bruh',
+      value: 78.9,
+    },
+    {
+      language: 'what',
+      value: 75.1,
+    },
+    {
+      language: 'why',
+      value: 68.0,
+    },
+    {
+      language: 'stahp',
+      value: 67.0,
+    },
+    {
+      language: 'no',
+      value: 65.6,
+    },
+    {
+      language: 'halp',
+      value: 65.1,
+    },
+    {
+      language: 'do',
+      value: 61.9,
+    },
+    {
+      language: 're',
+      value: 60.4,
+    },
+    {
+      language: 'mi',
+      value: 59.6,
+    },
+    {
+      language: 'fa',
+      value: 59.6,
+    }
+  ];
+
+
+var svg = d3.select("body")
+        .append("svg")
+        .attr("height", 700)
+        .attr("width", 700);
+
+
+
